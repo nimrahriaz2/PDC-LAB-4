28076f6f19731a36816a87124d9e72fa768e4753
nimrahriaz2 <nimrahriaz2@gmail.com>
2025-10-08 09:45:49 +0500

    Add files via upload

---
+++ Task 2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread foregroundThread = new Thread(ForegroundTask);
+        foregroundThread.IsBackground = false; 
+        foregroundThread.Start();
+
+      
+        Thread backgroundThread = new Thread(BackgroundTask);
+        backgroundThread.IsBackground = true;
+        backgroundThread.Start();
+
+        Console.WriteLine("Main thread finished execution.\n");
+    }
+
+   
+    static void ForegroundTask()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"[Foreground] Critical Task - Iteration {i}");
+            Thread.Sleep(500); 
+        }
+
+        Console.WriteLine("[Foreground] Critical Task Completed.\n");
+    }
+
+
+    static void BackgroundTask()
+    {
+        int i = 1;
+        while (true)
+        {
+            Console.WriteLine($"[Background] Non-Critical Task - Iteration {i++}");
+            Thread.Sleep(1000); 
+        }
+    }
+}
