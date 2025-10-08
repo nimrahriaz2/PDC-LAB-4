ae7f5b0ffb4a08aec1c99a7d5d2a967987cb29d1
nimrahriaz2 <nimrahriaz2@gmail.com>
2025-10-08 09:29:37 +0500

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+class ThreadLifecycleDemo
+{     static void Main()
+    {
+        Thread thread = new Thread(ThreadMethod);
+        Console.WriteLine( thread.ThreadState);
+        thread.Start();
+        Console.WriteLine(  thread.ThreadState);
+        thread.Join();
+        Console.WriteLine(thread.ThreadState);
+    
+    }
+    static void ThreadMethod()
+    {
+        Console.WriteLine(" Thread state: " + Thread.CurrentThread.ThreadState);
+        Console.WriteLine(thread.ThreadState);
+        Thread.Sleep(2000);
+    }
+}
